60e2959b8ce9360c108820a578b19e08fbf7880b
glebkruckov <glebkruchkov2004@gmail.com>
2025-04-29 15:24:26 +0300

    orders distribution

--- dat/db_fill.sql
+++
@@ -1,4 +0,0 @@
-BEGIN TRANSACTION;
-insert into `sample_model`(`id`,`count`,`description`) values(1, 100, 'Sample 1');
-insert into `sample_model`(`id`,`count`,`description`) values(2, 200, 'Sample 2');
-COMMIT;
